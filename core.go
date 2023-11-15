48c2a56997c2bd9f5b50d1302faba0a0aef3cd1d
Boss <cure1218@gmail.com>
2023-11-15 07:14:17 +0000

    boss

--- core.go
+++ core.go
@@ -2,42 +2,37 @@ package feature
 
 import (
 	"github.com/gin-gonic/gin"
-	dogmas "github.com/hexcraft-biz/envmod-dogmas"
 )
 
+// ================================================================
+//
+// ================================================================
 type Feature struct {
 	*gin.RouterGroup
-	Scopes []*dogmas.Scope
 }
 
 func New(e *gin.Engine, startsWith string) *Feature {
 	return &Feature{
 		RouterGroup: e.Group(startsWith),
-		Scopes:      []*dogmas.Scope{},
 	}
 }
 
-func (f *Feature) GET(relativePath string, scope *dogmas.Scope, handlers ...gin.HandlerFunc) gin.IRoutes {
-	f.Scopes = append(f.Scopes, scope)
-	return f.RouterGroup.GET(relativePath, handlers...)
-}
-
-func (f *Feature) POST(relativePath string, scope *dogmas.Scope, handlers ...gin.HandlerFunc) gin.IRoutes {
-	f.Scopes = append(f.Scopes, scope)
-	return f.RouterGroup.POST(relativePath, handlers...)
-}
-
-func (f *Feature) PUT(relativePath string, scope *dogmas.Scope, handlers ...gin.HandlerFunc) gin.IRoutes {
-	f.Scopes = append(f.Scopes, scope)
-	return f.RouterGroup.PUT(relativePath, handlers...)
-}
-
-func (f *Feature) PATCH(relativePath string, scope *dogmas.Scope, handlers ...gin.HandlerFunc) gin.IRoutes {
-	f.Scopes = append(f.Scopes, scope)
-	return f.RouterGroup.PATCH(relativePath, handlers...)
-}
-
-func (f *Feature) DELETE(relativePath string, scope *dogmas.Scope, handlers ...gin.HandlerFunc) gin.IRoutes {
-	f.Scopes = append(f.Scopes, scope)
-	return f.RouterGroup.DELETE(relativePath, handlers...)
-}
+//func (f *Feature) GET(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes {
+//	return f.RouterGroup.GET(relativePath, handlers...)
+//}
+//
+//func (f *Feature) POST(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes {
+//	return f.RouterGroup.POST(relativePath, handlers...)
+//}
+//
+//func (f *Feature) PUT(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes {
+//	return f.RouterGroup.PUT(relativePath, handlers...)
+//}
+//
+//func (f *Feature) PATCH(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes {
+//	return f.RouterGroup.PATCH(relativePath, handlers...)
+//}
+//
+//func (f *Feature) DELETE(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes {
+//	return f.RouterGroup.DELETE(relativePath, handlers...)
+//}
